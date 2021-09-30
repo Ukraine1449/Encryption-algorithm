98aa5fbb5fd43a453e070175bc6c0566e0f530f3
Ukraine1449 <59920083+Ukraine1449@users.noreply.github.com>
2021-09-29 21:30:08 -0400

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -8,6 +8,10 @@ def getSeed(passed):
             num = num + 90
     return num
 
+def antiPop(list):
+    tbr = list[0]
+    list.remove(list[0])
+    return tbr
 
 def encrypt(key, message):
     keylist = []
@@ -48,88 +52,156 @@ def encrypt(key, message):
     for i in messagelist:
         if i.lower() == "a":
             toreturn.append(A * mult)
-            toreturn.append("-")
         elif i.lower() == "b":
             toreturn.append(B * mult)
-            toreturn.append("-")
         elif i.lower() == "c":
             toreturn.append(C * mult)
-            toreturn.append("-")
         elif i.lower() == "d":
             toreturn.append(D * mult)
-            toreturn.append("-")
         elif i.lower() == "e":
             toreturn.append(E * mult)
-            toreturn.append("-")
         elif i.lower() == "f":
             toreturn.append(F * mult)
-            toreturn.append("-")
         elif i.lower() == "g":
             toreturn.append(G * mult)
-            toreturn.append("-")
         elif i.lower() == "h":
             toreturn.append(H * mult)
-            toreturn.append("-")
         elif i.lower() == "i":
             toreturn.append(I * mult)
-            toreturn.append("-")
         elif i.lower() == "j":
             toreturn.append(J * mult)
-            toreturn.append("-")
         elif i.lower() == "k":
             toreturn.append(K * mult)
-            toreturn.append("-")
         elif i.lower() == "l":
             toreturn.append(L * mult)
-            toreturn.append("-")
         elif i.lower() == "m":
             toreturn.append(M * mult)
-            toreturn.append("-")
         elif i.lower() == "n":
             toreturn.append(N * mult)
-            toreturn.append("-")
         elif i.lower() == "o":
             toreturn.append(O * mult)
-            toreturn.append("-")
         elif i.lower() == "p":
             toreturn.append(P * mult)
-            toreturn.append("-")
         elif i.lower() == "q":
             toreturn.append(Q * mult)
-            toreturn.append("-")
         elif i.lower() == "r":
             toreturn.append(R * mult)
-            toreturn.append("-")
         elif i.lower() == "s":
             toreturn.append(S * mult)
-            toreturn.append("-")
         elif i.lower() == "t":
             toreturn.append(T * mult)
-            toreturn.append("-")
         elif i.lower() == "u":
             toreturn.append(U * mult)
-            toreturn.append("-")
         elif i.lower() == "v":
             toreturn.append(V * mult)
-            toreturn.append("-")
         elif i.lower() == "w":
             toreturn.append(W * mult)
-            toreturn.append("-")
         elif i.lower() == "x":
             toreturn.append(X * mult)
-            toreturn.append("-")
         elif i.lower() == "y":
             toreturn.append(Y * mult)
-            toreturn.append("-")
         elif i.lower() == "z":
             toreturn.append(Z * mult)
-            toreturn.append("-")
         elif i == " ":
             toreturn.append(SP * mult)
-            toreturn.append("-")
     toreturn.append(mult)
     print(toreturn)
     return toreturn
+
+def decrypt(message):
+    A = 846
+    B = 30
+    C = 470
+    D = 476
+    E = 323
+    F = 740
+    G = 419
+    H = 485
+    I = 84
+    J = 165
+    K = 620
+    L = 245
+    M = 957
+    N = 331
+    O = 889
+    P = 329
+    Q = 244
+    R = 428
+    S = 494
+    T = 276
+    U = 37
+    V = 133
+    W = 595
+    X = 227
+    Y = 792
+    Z = 118
+    SP = 69
+    tbr = ""
+    mult = message.pop()
+    for i in range(len(message)):
+        numu = antiPop(message)
+        num = numu/mult
+        if num == A:
+            tbr = tbr + "a"
+        elif num == B:
+            tbr = tbr + "b"
+        elif num == C:
+            tbr = tbr + "C"
+        elif num == D:
+            tbr = tbr + "d"
+        elif num == E:
+            tbr = tbr + "e"
+        elif num == F:
+            tbr = tbr + "f"
+        elif num == G:
+            tbr = tbr + "g"
+        elif num == H:
+            tbr = tbr + "h"
+        elif num == I:
+            tbr = tbr + "i"
+        elif num == J:
+            tbr = tbr + "j"
+        elif num == K:
+            tbr = tbr + "k"
+        elif num == L:
+            tbr = tbr + "l"
+        elif num == M:
+            tbr = tbr + "m"
+        elif num == N:
+            tbr = tbr + "n"
+        elif num == O:
+            tbr = tbr + "o"
+        elif num == P:
+            tbr = tbr + "p"
+        elif num == Q:
+            tbr = tbr + "q"
+        elif num == R:
+            tbr = tbr + "r"
+        elif num == S:
+            tbr = tbr + "s"
+        elif num == T:
+            tbr = tbr + "t"
+        elif num == U:
+            tbr = tbr + "u"
+        elif num == V:
+            tbr = tbr + "v"
+        elif num == W:
+            tbr = tbr + "w"
+        elif num == X:
+            tbr = tbr + "x"
+        elif num == Y:
+            tbr = tbr + "y"
+        elif num == Z:
+            tbr = tbr + "z"
+        elif num == SP:
+            tbr = tbr + " "
+    return tbr
+
+
+
+#Yes ik about fucking dictionaries. Fuck off anyone reading this before i rework it.
+# To do is add more chars and dicts
+
 
 
 #print(encrypt("tEUhdy2zVYiE", "I like beans"))
